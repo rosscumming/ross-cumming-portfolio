09bc4d9a20a3eb1abfdcdd1d1929f7430298ca02
Ross <rosso5792@gmail.com>
2020-07-01 16:05:28 +0100

    adding google analytics

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import "./App.scss";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home";
+import ReactGA from "react-ga";
+ReactGA.initialize("UA-171409240-1");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const App = () => {
   return (
