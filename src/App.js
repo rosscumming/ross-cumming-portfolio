866d53db7bbf4668a92fb9b92e95fccfe966596b
Ross <rosso5792@gmail.com>
2020-07-01 16:14:11 +0100

    google analytics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.scss";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home";
 import ReactGA from "react-ga";
-ReactGA.initialize("UA-171409240-1");
-ReactGA.pageview(window.location.pathname + window.location.search);
+
+useEffect(() => {
+  ReactGA.initialize("UA-171409240-1");
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}, []);
 
 const App = () => {
   return (
